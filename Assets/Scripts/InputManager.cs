01b441babbe059747364768e9da90a3140139c5a
Chantelle Porritt <chantelleporritt@gmail.com>
2016-10-01 20:41:11 +0100

    Singleton

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : Singleton<InputManager>
+{
+	public enum Action
+	{
+		Horizontal,
+		Jump,	
+	};
+
+	//public void 
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Singleton<T> where T : class, new()
+{
+	private static T s_Instance;
+	public 	static T Instance
+	{
+		get
+		{
+			if(s_Instance == null)
+			{
+				s_Instance = new T();
+			}
+
+			return s_Instance;
+		}
+	}
+}
