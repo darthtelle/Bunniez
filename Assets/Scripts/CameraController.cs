9a048b42b93cd02983361baa93ba65ce9d52e695
Chantelle Porritt <chantelleporritt@gmail.com>
2016-10-01 14:07:07 +0100

    Camera and Extensions

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+	/*[SerializeField]
+	private GameObject[] m_PlayerList;*/ // TODO: For the future want to have multiple players.
+
+	[SerializeField]
+	private GameObject m_Player;
+
+	private Vector3 m_Offset;
+
+	private void Start()
+	{
+		m_Offset = gameObject.transform.position - m_Player.transform.position;
+	}
+
+	private void LateUpdate()
+	{
+		gameObject.transform.position = m_Player.transform.position + m_Offset;
+	}
+}
